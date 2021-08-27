df856bc8cc20f0014dd6baa2d4c7d836f786d4ab
Scott Herrmann - GitHub Hiddent Email <1109038+daborg69@users.noreply.github.com>
2021-08-26 20:08:14 -0400

    SlugCI Initialized

--- src/Slugent.ProcessQueueManager/MasterTasks.cs
+++ src/Slugent.ProcessQueueManager/MasterTasks.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SlugEnt.ProcessQueueManager;
 
-namespace Sample.ConcurEngine
+namespace SlugEnt.ProcessQueueManager
 {
+    /// <summary>
+    /// This is a Dictionary of ProcessingTasks (Reference type) with special Add and Clone methods to make adding tasks much simpler.
+    /// </summary>
     public class MasterTasks : Dictionary<int,ProcessingTask> {
         public MasterTasks () : base() {}
 
 
+        /// <summary>
+        /// Adds a new ProcessingTask to the Dictionary.
+        /// </summary>
+        /// <typeparam name="T">The Enum Id of the task</typeparam>
+        /// <param name="id">The enum value of the task</param>
+        /// <param name="speed">Whether this task is fast, moderate or long running</param>
+        /// <param name="methodToRun">The method that should be called when this task needs to run</param>
         public void Add<T> (T id, EnumProcessingTaskSpeed speed, Func<Object, bool> methodToRun) where T : Enum {
 
             int keyId = Convert.ToInt32(id);
@@ -26,6 +32,13 @@ namespace Sample.ConcurEngine
         }
 
 
+        /// <summary>
+        /// Clones the provided ProcessingTask that is stored in the dictionary
+        /// </summary>
+        /// <typeparam name="T">The enum class </typeparam>
+        /// <param name="id">The enum value to retrieve</param>
+        /// <param name="payload">An optional payload object that should be sent to the task when it is run</param>
+        /// <returns></returns>
         public ProcessingTask Clone<T> (T id, Object payload) where T : Enum {
             int keyId = Convert.ToInt32(id);
 
